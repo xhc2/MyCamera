413cd86c519f540fec72403c3703d19f0ae4ee01
xhc <1051261635@qq.com>
2015-12-16 22:06:41 +0800

    播放视频完成

--- app/src/main/java/com/example/tongmin/mycamera/PlayVideoActivity.java
+++ app/src/main/java/com/example/tongmin/mycamera/PlayVideoActivity.java
@@ -4,19 +4,24 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 import android.widget.VideoView;
 
 public class PlayVideoActivity extends AppCompatActivity {
 
-    private VideoView videoView;
+    private MyVideoView videoView;
     private String path;
+//    private Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_play_video);
-        videoView = (VideoView)findViewById(R.id.videoview);
+        videoView = (MyVideoView)findViewById(R.id.videoview);
+//        bt = (Button)findViewById(R.id.bt);
         Intent intent = getIntent();
         path = intent.getStringExtra("path");
         videoView.setVideoPath(path);
@@ -26,18 +31,27 @@ public class PlayVideoActivity extends AppCompatActivity {
 
             }
         });
-
+        videoView.start();
+//        bt.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                DisplayMetrics metrics = new DisplayMetrics(); getWindowManager().getDefaultDisplay().getMetrics(metrics);
+//                android.widget.FrameLayout.LayoutParams params = (android.widget.FrameLayout.LayoutParams) videoView.getLayoutParams();
+//                params.width =  metrics.widthPixels;
+//                params.height = metrics.heightPixels;
+//                params.leftMargin = 0;
+//                videoView.setLayoutParams(params);
+//                //试试能不能全屏
+//
+//            }
+//        });
     }
+
+
 
     @Override
     protected void onResume() {
         super.onResume();
-        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(
-                FrameLayout.LayoutParams.FILL_PARENT,
-                FrameLayout.LayoutParams.FILL_PARENT
-        );
-        //试试能不能全屏
-        videoView.setLayoutParams(lp);
-        videoView.start();
+
     }
 }
